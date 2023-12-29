2a3188deb6ec0fcc249d23943b1b20c6e7273b66
Mohak Agarwal <63978064+agarwalmohak6@users.noreply.github.com>
2023-12-29 15:56:18 +0530

    Route creation phase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import react from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -12,5 +14,19 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/conatct",
+    element: <Contact />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
